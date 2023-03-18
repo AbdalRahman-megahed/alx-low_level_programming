b9e70f3f02bdee7a69818d5cc78d926dbf9ef50c
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-18 08:28:56 -0700

    m

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+int x;
+for (n = 0; n < 9; n++)
+{
+for (x = 1; x < 10; x++)
+{
+if (x != n && n < x)
+{
+putchar(n + '0');
+putchar(x + '0');
+putchar(',');
+putchar(' ');
+}
+}
+}
