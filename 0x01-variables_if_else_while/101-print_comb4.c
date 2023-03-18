06469610a44aa4e218011db57c0b58c076b8318a
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-18 09:10:05 -0700

    m

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/*
- *11;rgb:0000/0000/0000
+/**
  * main - Entry point
  * Description: 'the program's description'
  * Return: Always 0 (Success)
