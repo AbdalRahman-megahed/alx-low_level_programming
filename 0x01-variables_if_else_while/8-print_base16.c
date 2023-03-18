9e9c7ba3d45c13f250f04d5fbc68356d21d46adf
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-17 19:13:24 -0700

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  /*main*/
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n + '0');
+}
+char x;
+for (x = 'a'; x <= f; x++)
+putchar(x);
+return (0);
+}
