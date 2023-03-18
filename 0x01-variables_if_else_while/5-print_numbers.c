bc0e5a7fb5dc0df21fdbd04f1c7ca39982ed058b
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-17 18:53:53 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  /*main*/
+for (n = 0; n < 10; n++)
+printf("%d", n);
+return (0);
+}
