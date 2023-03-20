c8cb824c1f078eecc8626de7ea3bf06004ef0305
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-19 18:48:04 -0700

    m

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <time.h>
+11;rgb:0000/0000/0000#include <time.h>
 #include <stdlib.h>
 /**
  * main - Entry point
@@ -9,39 +9,23 @@
 
 int main(void)
 {
-int n, x, y, z;
-for (n = 0; n < 10; n++)
+int num1;
+int num2;
+for (num1 = 0; num1 <= 99; num1++)
 {
-for (x = 0; x < 10; x++)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
-for (y = 0; y < 10; y++)
-{
-for (z = 0; z < 10; z++)
-{
-if (n + x <= y + z && y >= n)
-{
-/*never print again 00 00 or 55 55*/
-if (n == y && x == z)
-continue;
-putchar(n + '0');
-/* the last print 98 99*/
-if (n + y + z + x == 36 )
-continue;
-putchar(x + '0');
+putchar(num1 / 10 + '0');
+putchar(num1 % 10 + '0');
 putchar(' ');
-putchar(y + '0');
-putchar(z + '0');
-/* never print the last ,*/ 
-if (n + y + z + x == 35 && x == 8)
-continue;
+putchar(num2 / 10 + '0');
+putchar(num2 % 10 + '0');
+if (!(num1 == 98 && num2 == 99))
 {
 putchar(',');
 putchar(' ');
 }
 }
-}
-}
-}    
 }
 putchar('\n');
 return (0);
