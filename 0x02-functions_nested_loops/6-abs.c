07f721966e3f66dff1fe33d301551744cd683055
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-22 17:34:09 -0700

    m

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+11;rgb:0000/0000/0000
 /**
 * _abs - computes the absolute value of an integer
 * @n: the int to check
@@ -15,6 +15,7 @@ _putchar(n + '0');
 else
 {
 n = n * -1;
-_putchar(n + '0')
+_putchar(n + '0');
 }
+return (0);
 }
