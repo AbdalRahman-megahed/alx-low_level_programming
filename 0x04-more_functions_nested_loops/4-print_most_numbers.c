13e3041a4d313dc6cf0267d679d2b065e7b72989
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-23 16:49:13 -0700

    m

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+if (c == 2 || c == 4)
+continue;
+_putchar(c + '0');
+}
+_putchar('\n');
+}
