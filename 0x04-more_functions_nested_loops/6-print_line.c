a06797ea15a74876b8a81b6c0f6decd9fb4cb29f
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-23 17:44:28 -0700

    m

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,15 +5,16 @@
 */
 void print_line(int n)
 {
-int i;
 if (n <= 0)
 {
-continue;
+_putchar('\n');
+} else
+{
+int i;
 for (i = 1; i <= n; i++)
 {
 _putchar('_');
 }
 _putchar('\n');
 }
-_putchar('\n');
 }
