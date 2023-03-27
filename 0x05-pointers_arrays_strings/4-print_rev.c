b67db5a76c9567a809044503d059d54295952ffb
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-27 16:13:20 -0700

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_rev - imprime en reversa
+* @s: string
+* return: 0
+*/
+void print_rev(char *s)
+{
+int c = 0;
+while (*s != '\0')
+{
+*s++;
+c++;
+}
+while (c != 0)
+{
+_putchar(--*s);
+ c--;
+}
+}
