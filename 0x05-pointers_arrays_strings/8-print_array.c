45f0ab834bf5ba70b8b462ae279291790901bd27
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-03-27 19:00:58 -0700

    m

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+11;rgb:0000/0000/0000#include "main.h"
 /**
 * print_array - a function that prints n elements of an array
 * @a: array name
@@ -17,4 +17,5 @@ continue;
 printf(",");
 printf(" ");
 }
+printf("\n");
 }
