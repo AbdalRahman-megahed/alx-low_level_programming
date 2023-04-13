289655eb17bb7b219e523ffbedca73634eb1ceab
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-04-12 20:56:48 -0700

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,6 +4,7 @@
 * null byte, to the end of the string pointed to by @dest.
 * @dest: A pointer to the string to be concatenated upon.
 * @src: The source string to be appended to @dest.
+*
 * Return: A pointer to the destination string @dest.
 */
 char *_strcat(char *dest, char *src)
