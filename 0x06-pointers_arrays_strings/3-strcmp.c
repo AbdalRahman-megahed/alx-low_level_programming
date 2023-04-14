551bcea115f08b85711e55537ae05ab0a10d3cb5
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-04-14 04:26:13 -0700

    m

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i] == s2[i])
-		i++;
-	if (s1 == s2)
-		i = 0;
-	return (s1[i] - s2[i]);
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
