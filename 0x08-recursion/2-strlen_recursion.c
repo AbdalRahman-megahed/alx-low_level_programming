a6e119252ab5020797dbcfe57014b9f3be7c77e9
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-04-19 02:08:13 -0700

    m

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,7 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		for (int i = s; i != 0; i--);
-		return (i);
-	}
-	s++;
-	_strlen_recursion(s);
+	if (*s != '\0')
+		_strlen_recursion(s + 1);
+	return (int *s);
 }
