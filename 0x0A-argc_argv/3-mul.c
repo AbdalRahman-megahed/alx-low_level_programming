b510b371c26b1170c2b39aa347300041c16e54fd
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-04-20 02:12:51 -0700

    m

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints the program name.
+* @argc: int.
+* @argv: list.
+* Return: 0.
+*/
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+	return (0);
+}
