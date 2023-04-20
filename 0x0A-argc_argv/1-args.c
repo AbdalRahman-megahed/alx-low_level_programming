a59596d4d5f8b2499abc66bcb84e260a55aec169
AbdalRahman-megahed <abdo2003amer@gmail.com>
2023-04-20 01:41:16 -0700

    m

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints the program name.
- * @argc: int.
- * @argv: list.
- * Return: 0.
- */
-int main(int argc, char *argv[] __attribute__((unused)))
+* main - prints the program name.
+* @argc: int.
+* @argv: list.
+* Return: 0.
+*/
+int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
