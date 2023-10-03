35dd4d9dd9843b397b9c4a77c3ca7fe2871c4be5
AbdalRahman-megahed <71913604+AbdalRahman-megahed@users.noreply.github.com>
2023-10-04 01:40:36 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,10 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
